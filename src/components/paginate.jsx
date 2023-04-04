2552267b5394ce6019df8d8404ebfb1f07d383a8
Mba Nenye <mbachinenye75@gmail.com>
2023-04-04 06:42:20 -0700

    update

--- src/components/paginate.jsx
+++ src/components/paginate.jsx
@@ -8,7 +8,7 @@ function Paginate({countryPerPage,totalCountry,setCurrentPage}) {
         <ul className="flex">
     {pageNumbers.map(number => (
         <li className="flex rounded shadow" key={number}>
-            <button className=" md:flex justify-between md:m-4 md:p-4 rounded m- p-2 font-bold" onClick={()=>setCurrentPage(number)}> {number}</button>
+            <button className=" md:flex justify-between md:m-4 md:p-4 rounded m- p-2 font-bold dark:bg-white dark:text-white" onClick={()=>setCurrentPage(number)}> {number}</button>
         </li>
     ))}
         </ul>
